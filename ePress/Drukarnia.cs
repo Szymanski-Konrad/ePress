0ea9df17236d6678d617a9122610840cf63b9ab3
hunte <hunte@DESKTOP-PG8VCPB>
2017-05-24 18:41:11 +0200

    ten

---
+++ ePress/Drukarnia.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ePress
+{
+    class Drukarnia
+    {
+        public int wydajnosc { get; set; }
+        public int jakosc { get; set; }
+        List<Zlecenie> kolejka;
+        List<Produkty> coDrukuje;
+
+        public Drukarnia()
+        {
+            Random r = new Random();
+            wydajnosc = r.Next(3000, 4000);
+            jakosc = r.Next(1, 10);
+            kolejka = new List<Zlecenie>();
+            coDrukuje = new List<Produkty>();
+        }
+
+        public void DodajZlecenie(Zlecenie z)
+        {
+            kolejka.Add(z);
+        }
+
+        public void ZglaszanieWydruku()
+        {
+
+        }
+
+        public void CzasWydruku()
+        {
+
+        }
+
+
+    }
+}
---
+++ ePress/Wydawnictwo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ePress
+{
+    class Wydawnictwo
+    {
+        List<Drukarnia> drukarnie;
+        List<Zlecenie> zlecenia;
+        double saldo;
+
+        public Wydawnictwo()
+        {
+            drukarnie = new List<Drukarnia>();
+            zlecenia = new List<Zlecenie>();
+            saldo = 10000;
+        }
+
+        public void KupDrukarnie()
+        {
+            Random r = new Random();
+            Drukarnia d = new Drukarnia();
+            double cena = 2000 * (d.jakosc / 5) * (d.wydajnosc / 3500);
+            saldo -= cena;
+            drukarnie.Add(d);
+        }
+
+        public void CzytajKsiazke(Zlecenie z)
+        {
+            
+        }
+
+        public void PrzyjmijZamowienie(Zlecenie z) 
+        {
+            zlecenia.Add(z);
+        }
+    }
+}
